16821f54f2abbd83a86665968cf28daf41f9ced4
GalynaM <gmalinskaya@gmail.com>
2021-09-26 00:05:41 -0700

    Tectonic Data is added to Map

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,9 +1,12 @@
 // Specify url to get data about all earthquakes for the last month
 all_earth_q_lastMonth_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson" 
 
+
+// Specify url to get data about tectonic plates
+tectonic_plates_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json" 
+
 // A function to determine the marker size based on the eartchquake magnitude
 function markerSize(magnitude) {
-    // return Math.sqrt(magnitude) * 50;
     return magnitude * 10000;
 }
 
@@ -14,58 +17,64 @@ function markerColor(depth){
            depth > 50  ? '#ffc015' :
            depth > 30  ?  '#ffde59':
            depth > 10   ? '#d0e606':
-          //  depth > -10   ? 
            '#5ca904';
 }
 
 // Use D3 to get JSON data
-d3.json(all_earth_q_lastMonth_url).then(function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function.
-    createFeatures(data.features);
+d3.json(all_earth_q_lastMonth_url).then(function (earthquakeData) {
+  d3.json(tectonic_plates_url).then(function (tectonicData) {
+      // Once we get a response, send the data.features object to the createFeatures function.
+      
+      createFeatures(earthquakeData.features, tectonicData.features);
+      console.log(tectonicData)
+  })
 });
 
 
-function createFeatures(earthquakeData) {
+function createFeatures(earthquakeData, tectonicPlatesData) {
 
   // Create a GeoJSON layer that contains the features array on the earthquakeData object.
   // Using pointToLayer function for each feature in geoJSON layer add Circle Points with
   // customized radius, color, binded Tooltip and Popup
-  var earthquakes = L.geoJSON(earthquakeData, {
-      pointToLayer: function(feature, latlng){
-      
-          // define variables too style Circle Markers
-          var magnitude = feature.properties.mag;
-          var depth = feature.geometry.coordinates[2];
-          var place = feature.properties.place;
-          var location = (place.split(",")[1]===undefined) ? place : place.split(",")[1];
 
-          return L.circle(latlng,
-              {
-                  radius: markerSize(magnitude),
-                  color: "#353535",
-                  weight: 1,
-                  fillColor: markerColor(depth),
-                  weight: 1,
-                  opacity: 1,
-                  fillOpacity: 0.8
-              }).bindTooltip(`<p>Mag: ${magnitude}</p>
-              <p>Depth: ${depth}</p>
-              <p>Loc: ${location}</p>`)
-              .bindPopup(`<h3>${place}</h3><hr><p>${new Date(feature.properties.time)}</p>`)
-      }
-  });
+  var tectonicPlates = L.geoJSON(tectonicPlatesData)
+
+  var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function(feature, latlng){
+    
+        // define variables to style Circle Markers
+        var magnitude = feature.properties.mag;
+        var depth = feature.geometry.coordinates[2];
+        var place = feature.properties.place;
+        var location = (place.split(",")[1]===undefined) ? place : place.split(",")[1];
+
+        return L.circle(latlng,
+            {
+                radius: markerSize(magnitude),
+                color: "#353535",
+                weight: 1,
+                fillColor: markerColor(depth),
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            }).bindTooltip(`<p>Mag: ${magnitude}</p>
+            <p>Depth: ${depth}</p>
+            <p>Loc: ${location}</p>`)
+            .bindPopup(`<h3>${place}</h3><hr><p>${new Date(feature.properties.time)}</p>`)
+    }
+});
 
   // Send our earthquakes layer to the createMap function
-  createMap(earthquakes);
+  createMap(earthquakes, tectonicPlates);
 
 }
 
-function createMap(earthquakes) {
+function createMap(earthquakes, tectonicPlates) {
 
     // Create the base layers.
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
+    // var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // })
   
     var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
@@ -73,13 +82,14 @@ function createMap(earthquakes) {
 
     // Create a baseMaps object.
     var baseMaps = {
-      "Street Map": street,
-      // "Topographic Map": topo
+      // "Street Map": street,
+      "Topographic Map": topo
     };
   
     // Create an overlay object to hold our overlay.
     var overlayMaps = {
-      Earthquakes: earthquakes
+      Earthquakes: earthquakes,
+      TectonicPlates: tectonicPlates
     };
   
     // Create our map, giving it the streetmap and earthquakes layers to display on load.
@@ -88,15 +98,15 @@ function createMap(earthquakes) {
         37.09, -95.71
       ],
       zoom: 5,
-      layers: [street, earthquakes]
+      layers: [topo, tectonicPlates]
     });
   
     // Create a layer control.
     // Pass it our baseMaps and overlayMaps.
     // Add the layer control to the map.
-    // L.control.layers(baseMaps, overlayMaps, {
-    //   collapsed: false
-    // }).addTo(myMap);
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
 
     // Add legend
     var legend = L.control({position: 'bottomright'});
