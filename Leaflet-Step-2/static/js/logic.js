24276af05739cb92eb773d3b20a1e52895d64d86
GalynaM <gmalinskaya@gmail.com>
2021-09-30 10:06:59 -0700

    Part 2 added

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -7,7 +7,7 @@ tectonic_plates_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/m
 
 // A function to determine the marker size based on the eartchquake magnitude
 function markerSize(magnitude) {
-    return magnitude * 10000;
+    return magnitude * 20000;
 }
 
 // A function to determine the marker color based on the eartchquake depth
@@ -33,16 +33,16 @@ d3.json(all_earth_q_lastMonth_url).then(function (earthquakeData) {
 
 function createFeatures(earthquakeData, tectonicPlatesData) {
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Using pointToLayer function for each feature in geoJSON layer add Circle Points with
-  // customized radius, color, binded Tooltip and Popup
-
+  // Create a GeoJSON layer that contains the features array on the tectonicPlatesData object.
   var tectonicPlates = L.geoJSON(tectonicPlatesData, {
     style: {
       color: 'yellow'
     }
   })
 
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Using pointToLayer function for each feature in geoJSON layer add Circle Points with
+  // customized radius, color, binded Tooltip and Popup
   var earthquakes = L.geoJSON(earthquakeData, {
     pointToLayer: function(feature, latlng){
     
@@ -68,26 +68,38 @@ function createFeatures(earthquakeData, tectonicPlatesData) {
     }
 });
 
-  // Send our earthquakes layer to the createMap function
+  // Send our earthquakes and tectonicPlates layers to the createMap function
   createMap(earthquakes, tectonicPlates);
 
 }
 
+// Function to create Map with Base and Overlay Layers, and Legend
 function createMap(earthquakes, tectonicPlates) {
 
     // Create the base layers.
-    // var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    // })
-  
-    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    var googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+    });
+
+    var stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	    subdomains: 'abcd',
+	    minZoom: 1,
+	    maxZoom: 16,
+	    ext: 'jpg'
+    });
+
+    var satelite = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
+      maxZoom: 20,
+      attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
     });
 
     // Create a baseMaps object.
     var baseMaps = {
-      // "Street Map": street,
-      "Topographic Map": topo
+      "Satelite": satelite,
+      "Terrain": googleTerrain,
+      "Watercolor": stamen_Watercolor
     };
   
     // Create an overlay object to hold our overlay.
@@ -96,13 +108,13 @@ function createMap(earthquakes, tectonicPlates) {
       TectonicPlates: tectonicPlates
     };
   
-    // Create our map, giving it the streetmap and earthquakes layers to display on load.
+    // Create our map, giving it the satelite map and earthquakes, tectonic layers to display on load.
     var myMap = L.map("map", {
       center: [
         37.09, -95.71
       ],
-      zoom: 5,
-      layers: [topo, tectonicPlates]
+      zoom: 4,
+      layers: [satelite, earthquakes, tectonicPlates]
     });
   
     // Create a layer control.
